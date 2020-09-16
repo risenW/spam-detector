534d7d6fd3997e95e72c0ac220fca7775bc5c8d2
Rising Odegua <risingodegua@gmail.com>
2020-09-16 16:18:29 +0100

    predict on sms using saved model

--- src/app.py
+++ src/app.py
@@ -1,70 +1,50 @@
-from flask import Flask, render_template, flash, redirect, request, url_for, jsonify, make_response
-import json
+from flask import Flask, render_template, make_response
 from twilio.rest import Client
-import os
 from dotenv import load_dotenv
-from model import load_model
+from model import load_model,  process_sms
 
-load_dotenv()
-
-
-app = Flask(__name__)
+import warnings
+warnings.filterwarnings("ignore")
 
 TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
 TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
 twilio_api = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+load_dotenv()
+model =load_model() ## load saved model
+
+
+app = Flask(__name__)
+
+
 def fetch_sms():
     return twilio_api.messages.stream()
 
+sms = fetch_sms() ## get all sms from twilio
+
+
 @app.route("/")
 def index():
-    spam_or_not = [1, 0]
-    sms = [{"body":"d ajadnlanldknaldklakdlakmdlakd"}, {"body": "loreaadkajdkakankdnakjdnkajndkd not spam"}]
-    # sms = fetch_sms()
-    # print(next(sms))
-    # model =load_model()
     spam_msg = []
     not_spam_msg = []
 
-    for i, message in enumerate(sms):
-        if spam_or_not[i] == 1:
-            spam_msg.append(message)
-        else:
-            not_spam_msg.append(message)
+    for msg in sms:
+        txt = msg.body
+        txt = process_sms(txt)
+        pred = model.predict_classes(txt).item()
 
-    # spam_or_not.append(model.predict(message))
+        if pred == 1:
+            spam_msg.append(msg)
+        else:
+            not_spam_msg.append(msg)
+
 
     template = render_template('index.html', spam_msg=spam_msg, not_spam_msg=not_spam_msg)
     response = make_response(template)
-    # response.headers['Cache-Control'] = 'public, max-age=300, s-maxage=600'
+    response.headers['Cache-Control'] = 'public, max-age=300, s-maxage=600'
     return response
 
 
-
-
-
-# @app.route("/about")
-# def about():
-#     return render_template('about.html')
-
-
-# @app.route("/scrap",  methods=['POST'])
-# def scraptweets():
-#     options = {}
-#     if request.method == "POST":
-#         req = request.form
-#         print(req)
-#         for val in req:
-#             print(req[val])
-#             if req[val] != "":
-#                 options[val] = req[val]
-#     # print(options)
-#     download_path = scrap(options)
-#     return render_template('success.html', path=download_path)
-
-
-# app.secret_key = "nlhkjtgjhfhvhjfyfgcjgdtdgcngcghdt"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000, debug=True)
--- src/model.py
+++ src/model.py
@@ -1,3 +1,24 @@
+import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+with open('spam_model/tokenizer.pkl', 'rb') as input:
+    tokenizer = pickle.load(input)
+ 
 
 def load_model():
-    return "Model"
+    model = tf.keras.models.load_model("spam_model")
+    return model
+
+
+def process_sms(sms):
+    '''
+    Apply saved tokenizer to new sms text and returns processed data
+    '''
+    max_length = 10
+    sms = [sms]
+    sms_proc = tokenizer.texts_to_sequences(sms)
+    sms_proc = pad_sequences(sms_proc, maxlen=max_length, padding='post')
+    
+    return sms_proc
